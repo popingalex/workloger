cc3dd3cb4eb097f8092abf671d7ac087f21d5e89
Alexander <zhe009@hotmail.com>
2018-10-29 23:41:41 +0800

    update

--- util/robot.py
+++ util/robot.py
@@ -6,6 +6,8 @@ import json
 import re
 
 URL_ROOT = 'http://106.37.227.19:7003'
+PATH_MAIN = 'ams/ams_weekly/WeeklyweeklyBrowse.do'
+# PATH_MAIN = 'ams/ams_weekly/AnaphaseTreatmentBrowse.do'
 PATH_LOGIN = 'ams/util/sys/login.do?method=login&username={}&pwd={}'
 PATH_DETAIL = 'ams/ams_weekly/WeeklyweeklyDisplay.do?weeklyweeklyid={}'
 PATH_HISTORY = 'ams/ams_weekly/WeeklyweeklyBrowse.do?flag=true'
@@ -20,15 +22,16 @@ PATTERN_TIMES = {
     'over_end_time': r'var oset = \'(?P<time>[-\w: ,]+)\''
 }
 PATTERN_PROJECT_NAME = "nodes\['(?P<key>[a-z0-9]{32})'\] = new xyTree.NodeNormal\('(?P<name>.*)'\);.*"
-
+from time import sleep
 
 def foreground():
     pass
 
-
+requests.adapters.DEFAULT_RETRIES = 5
 class Background:
     def __init__(self):
         print('username : {}'.format(profile['username']))
+
         self.session = requests.session()
 
     def login(self):
@@ -37,6 +40,8 @@ class Background:
         print(response)
         if response.status_code == 200 and "toUrl:'ams_weekly/AnaphaseTreatmentBrowse.do'" in response.content.decode():
             print('login success')
+            # 必须登陆一下这个页面才行
+            self.session.get('{}/{}'.format(URL_ROOT, PATH_MAIN), params={'flag': 'true'}, timeout=10)
         else:
             print('login faield: {}'.format(response.content.decode()))
             exit(1)
@@ -101,31 +106,30 @@ class Background:
             # 'overtimeend': over_end,
             # 'overstartstr': over_start,
             # 'overendstr': over_end,
-            'btnSave': 'clicked',
             'otherprojectid': '',
             'plancontent': '',
             'problem': '',
             'remark': '',
             'btnAdd': '',
             'btnBack': '',
+            'btnSave': 'clicked',
         })
         response = self.session.post('{}/{}'.format(URL_ROOT, PATH_LOG), data=data, headers={
             'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': str(len(data)),
-            'Referer': 'http://192.168.29.31:7003/ams/ams_weekly/WeeklyweeklyAdd.do',
+            'Referer': 'http://106.37.227.19:7003/ams/ams_weekly/WeeklyweeklyAdd.do',
             'Host': '106.37.227.19'
-        })
-        print(response.content)
+        }, allow_redirects=False)
+        print(response)
+        print(response.content.decode())
 
     def demo(self):
         self.login()
-        # for detail in self.query(username='许文哲', begin='2018-03-08', end='2018-03-10'):
-        #     print(detail)
 
-        for detail in self.query(username='许文哲', begin='2018-10-20', end='2018-10-30'):
-            print(detail)
         project = self.project('工程效率改进平台')
         self.log(project_key=project[0]['key'], project_name=project[0]['name'],
                  content="工程效率平台工作内容整理",
-                 start="2018-10-23 06:00", end="2018-10-23 18:00")
+                 start="2018-10-28 15:00", end="2018-10-28 18:00")
+        for detail in self.query(username='许文哲', begin='2018-10-28', end='2018-10-30'):
+            print(detail)
         pass
